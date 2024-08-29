0f3fd03ab7081471b9e3141511167180e076d551
Cura <adrian.cura.dev@gmail.com>
2024-08-28 23:48:49 -0300

    make first letter as capital for workspaces pages to use useEffect

--- app/(routes)/workspace/personal/[userId]/[workspaceId]/page.tsx
+++ app/(routes)/workspace/personal/[userId]/[workspaceId]/page.tsx
@@ -5,7 +5,7 @@ import SideNav from "@/components/layout/SideNav";
 import WorkspaceHero from "@/components/layout/WorkspaceHero";
 import React, { useState } from "react";
 
-const page = ({ params }: { params: ParamsProps }) => {
+const PersonalPage = ({ params }: { params: ParamsProps }) => {
   const [display, setDisplay] = useState(true);
 
   return (
@@ -16,4 +16,4 @@ const page = ({ params }: { params: ParamsProps }) => {
   );
 };
 
-export default page;
+export default PersonalPage;
